fc573115b2ab58851fd8b2e8a1a5694563f2da3b
CVO script002 <script002@SJCA.LAN>
2018-12-18 21:37:31 +0100

    Eerste commit van project

---
+++ src/layouts/AppHeader.vue
@@ -0,0 +1,45 @@
+<template>
+    <nav class="app-header">
+        <h1>Beau-tique</h1>
+        <div class="nav-groep">
+            <router-link to="/" exact>Home</router-link>
+            <router-link to="/producten">Producten</router-link>
+            <router-link to="/over">Over ons</router-link>
+            <router-link to="/contact">Contacteren</router-link>
+        </div>
+    </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+    .app-header {
+        display: flex;
+        justify-content: space-between;
+        background-color: #40b782;
+        height: 6rem;
+        align-items: center;
+        padding: 0 3rem;
+        color: white;
+    }
+
+    .nav-groep a {
+        margin: 0 1.5rem;
+        text-transform: uppercase;
+        font-weight: bold;
+        color: #b3f7d8;
+        transition: color 0.3s;
+    }
+
+    .nav-groep a.router-link-active {
+        color: white;
+    }
+
+    .nav-groep a:hover {
+        color: white;
+    }
+</style>
---
+++ src/views/Over.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="over">
+        <h1>Over ons</h1>
+        <article>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum eligendi cumque natus fugit! Suscipit nam consequuntur voluptatum corporis eveniet aliquid, earum placeat ea sint quidem repudiandae facere nihil quaerat voluptates!
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure obcaecati deleniti debitis reiciendis harum mollitia? Adipisci eligendi natus quod dolore laudantium eum praesentium alias et dicta, amet impedit, deserunt ipsam tempore veniam, quae eveniet blanditiis? Adipisci dolorem cupiditate est commodi omnis beatae voluptatum, praesentium odio minus, porro, dolor odit. Libero mollitia fugit dolorum. Animi, autem dolor minus inventore eaque odit harum vero accusantium! Omnis deleniti eaque nihil ex iusto ipsam in nesciunt cupiditate quasi, sunt doloremque earum assumenda asperiores beatae dolorem nam repellat recusandae libero repellendus aperiam? Totam consequuntur provident quae cupiditate at, hic earum labore sapiente culpa, repellat nam.
+            </p>
+            <hr>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum eligendi cumque natus fugit! Suscipit nam consequuntur voluptatum corporis eveniet aliquid, earum placeat ea sint quidem repudiandae facere nihil quaerat voluptates!
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eum eveniet deserunt illum quis commodi non natus dolorem ut necessitatibus repudiandae id et voluptatem voluptates iste, modi aliquid vitae quibusdam magni odit! Quisquam blanditiis pariatur voluptatum a, velit voluptatem totam atque delectus eum fugiat rerum assumenda, provident qui aspernatur. Architecto at quas, totam animi nemo perspiciatis porro magnam consequatur corrupti aperiam.
+            </p>
+        </article>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+    .over {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        padding-top: 5rem;
+    }
+
+    article {
+        padding: 3rem;
+        text-align: justify;
+        max-width: 1152px;
+        line-height: 1.6;
+    }
+
+    h1 {
+        font-size: 7rem;
+        color: #40b782;
+    }
+
+    hr {
+        margin: 2rem 0;
+    }
+</style>
